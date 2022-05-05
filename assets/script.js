81ddcc39f680d6c6e0bfb6067fa7aba4c5405e18
fanylouise <gandra.fany@gmail.com>
2022-05-05 20:34:58 -0300

    stage 3 +- complete

--- assets/script.js
+++ assets/script.js
@@ -14,3 +14,15 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+ScrollReveal({
+  origin: 'top',
+  distance:'30px',
+  durantion:700,
+}).reveal(`
+#home,
+#home img,
+#home .stats,
+#assignments,
+#assignments header,
+#assignments .cards
+   `);
